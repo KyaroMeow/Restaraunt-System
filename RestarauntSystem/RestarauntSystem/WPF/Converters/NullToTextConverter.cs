2c86484b0f786907f10d23c4235fec38708dbe6b
KyaroMeow <164043414+KyaroMeow@users.noreply.github.com>
2025-06-25 15:29:43 +0300

    views

---
+++ RestarauntSystem/RestarauntSystem/WPF/Converters/NullToTextConverter.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Globalization;
+using System.Windows.Data;
+
+namespace RestarauntSystem.WPF.Converters
+{
+    public class NullToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string[] texts = parameter.ToString().Split('|');
+            return value == null ? texts[0] : texts[1];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
